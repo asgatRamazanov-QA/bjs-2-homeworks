38d9c0207082a8783b9b30c4b2d352348d6b060f
asgat_frontend <asr213@mail.ru>
2023-11-22 22:44:24 +0300

    Первое задание №1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,14 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		arr = [];
+	} else if (d === 0) {
+		arr = [-b / 2 * a];
+	} else if (d > 0) {
+		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)]
+	}
+	return arr;
 }
