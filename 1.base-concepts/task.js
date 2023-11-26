e3917ba45a5cb2fb36e8c8861db7c6eac1e9bf06
asgat_frontend <asr213@mail.ru>
2023-11-26 20:16:13 +0300

    Первое и второе задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,7 +2,7 @@
 
 function solveEquation(a, b, c) {
 	let arr = [];
-	let d = b ** 2 - 4 * a * c;
+	const d = b ** 2 - 4 * a * c;
 	if (d < 0) {
 		arr = [];
 	} else if (d === 0) {
@@ -12,3 +12,19 @@ function solveEquation(a, b, c) {
 	}
 	return arr;
 }
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {  
+	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)){
+	  return false;
+	}
+  
+	percent = percent/100/12;
+  
+	let loanBody = amount - contribution;
+	let monthlyPayment = loanBody * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+	let totalPayment = monthlyPayment * countMonths;
+  
+	totalPayment = Number(totalPayment.toFixed(2));
+  
+	return totalPayment;
+  }
