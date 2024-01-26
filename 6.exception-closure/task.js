aa6b568d6874e6028e2c5a204ae133a57a8ac7d9
asgat_frontend <asr213@mail.ru>
2024-01-26 18:56:29 +0300

    Обработка исключений и замыкания готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount(parse) {
+	let number = Number.parseFloat(parse);
+	if (Number.isNaN(Number.parseFloat(parse))) {
+		throw new Error("Невалидное значение");
+	}
+	return number;
+}
+
+function validateCount(parse) {
+	try {
+		return parseCount(parse);
+	} catch (error) {
+		return (error);
+	}
+}
+// вторая задача
+
+class Triangle {
+	constructor(a, b, c) { 
+        this.triangleError = new Error("Треугольник с такими сторонами не существует");
+		this.sideA = a;
+		this.sideB = b;
+		this.sideC = c;
+		if (this.sideA + this.sideB < this.sideC || this.sideB + this.sideC < this.sideA || this.sideA + this.sideC < this.sideB) {
+			throw this.triangleError;
+		}
+	}
+	get perimeter() {
+        let Perimeter= this.sideC + this.sideB + this.sideA;
+		return Perimeter;
+	}
+	get area() {
+		const p = this.perimeter / 2;
+		let square = Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC)).toFixed(3);
+		return Number.parseFloat(square);
+	}
+	
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+            return "Ошибка! Треугольник не существует"
+            },
+            get perimeter() {
+             return "Ошибка! Треугольник не существует"
+            }
+        }
+    }
+}
